e42d17d8999ab8cb80a09f87568eaad2cf0eb279
veda <vedaandrew@gmail.com>
2018-08-14 00:11:16 +0100

    Revert "SpringBoot2 with JPA and ReactiveWeb"
    This reverts commit 6f029e8

--- src/main/java/com/lly/alertMgmt/repository/AlertDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lly.alertMgmt.repository;
-
-import com.lly.alertMgmt.domain.AlertData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertDataRepository extends JpaRepository<AlertData, Long> {
-    //
-}
